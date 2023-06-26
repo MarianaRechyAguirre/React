b4c5cd01f0312f1d91992e2a06e5258b14beb07e
marianarechy <mrechy@griddynamics.com>
2023-06-26 15:49:48 -0600

    cards-info

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,14 +1,35 @@
-import rectangle from "../images/feature/rectangle.png"
 
-export default function FeatureCard(){
-    return (
-      <div className="w-[340px] h-[552px] bg-pink-500 ml-[149px] mt-[25px]">
-        <div className="w-[340px] h-[382px]">
-        <img src={rectangle} alt="logo"></img>
-        </div>
-        <div className="w-[340px] h-[72px] mt-[24px] bg-pink-300 mb-[24px]"></div>
-        <div className="w-[340px] h-[50px] bg-blue-200"></div>
-      </div>
-    );
-  }
+export default function FeatureCard(props: {
+  text: string;
+  price: string;
+  picture: string;
+  name: string;
+  city: string;
+  icon: string;
+}) {
   
+  return (
+    <div className="w-[340px] h-[552px] mt-[25px] mr-[40px]">
+      <div className="w-[340px] h-[382px]">
+      <img src={props.picture} alt="bg"></img>
+      </div>
+      <div className="w-[340px] h-[72px] mt-[24px] mb-[24px] flex flex-col justify-between">
+        <p className=" text-neutral-900 text-[24px] font-medium">
+          {props.text}
+        </p>
+        <p className=" text-neutral-700 text-[20px] font-medium">
+          {props.price}
+        </p>
+      </div>
+      <div className="w-[340px] h-[50px] flex flex-row items-center">
+        <div className="w-[40px] h-[40px]">
+        <img src={props.icon} alt="icon"></img>
+        </div>
+        <div className="w-[284px] h-[50px] ml-[16px]">
+          <p className="text-neutral-900 text-[18px]">{props.name}</p>
+          <p className="text-neutral-500 text-[14px]">{props.city}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/FeaturedCards.tsx
+++ src/components/FeaturedCards.tsx
@@ -1,10 +1,48 @@
 import FeatureCard from "./FeatureCard";
+import rectangle from "../images/feature/rectangle.png";
+import one from "../images/feature/one.png";
+import two from "../images/feature/two.png";
+import three from "../images/feature/three.png";
+import mona from "../images/feature/mona.png"
+import lisa from "../images/feature/lisa.png"
+import robert from "../images/feature/robert.png"
+import deniro from "../images/feature/deniro.png"
 
-export default function FeaturedCards(){
-    return (
-      <div className="w-[1291px] h-[585px] bg-pink-200 ml-[149px] mt-[25px]">
-        <FeatureCard />
-      </div>
-    );
-  }
-  
+export default function FeaturedCards() {
+  return (
+    <div className="w-[1291px] h-[585px] ml-[149px] mt-[25px] flex flex-row">
+      <FeatureCard
+        text="Roselands House"
+        price="$35,000.000"
+        picture={rectangle}
+        name="Dianne Russell"
+        city="Manchester, Kentuky"
+        icon={mona}
+      />
+      <FeatureCard
+        text="Woodlandside"
+        price="$20,000.000"
+        picture={one}
+        name="Robert Fox"
+        city="Dr. San Jose, South Dakota"
+        icon={robert}
+      />
+      <FeatureCard
+        text="The Old Ligthouse"
+        price="$44,000.000"
+        picture={two}
+        name="Ronald Richards"
+        city="Santa Ana, Illinois"
+        icon={deniro}
+      />
+      <FeatureCard
+        text="Cosmo's House"
+        price="$22,000.000"
+        picture={three}
+        name="Jenny Wilson"
+        city="Preston Rd. Inglewood, Maine 98380"
+        icon={lisa}
+      />
+    </div>
+  );
+}

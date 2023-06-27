9f991e7388ed1d33f672a21e5d4f8a9f4f080e98
marianarechy <mrechy@griddynamics.com>
2023-06-27 14:52:15 -0600

    review

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Review from "./components/Review";
 import SectionFeatured from "./components/SectionFeatured";
 import SectionFind from "./components/SectionFind";
 import SectionTour from "./components/SectionTour";
@@ -14,6 +15,7 @@ export default function App() {
       <div className="hope">
       <SectionFeatured />
       <SectionTour />
+      <Review />
       </div>
     </div>
   );
---
+++ src/components/Review.tsx
@@ -0,0 +1,10 @@
+export default function Review() {
+    return (
+      <div className="w-full h-[664px] bg-yellow-200">
+        <p className="text-[14px] font-medium text-center text-[#F59E0B]">See Our Review</p>
+        <p className="text-[32px] font-semibold text-center mb-[40px]">What Our User Say About Us</p>
+        <div className="w-full h-[505px] bg-green-200"></div>
+      </div>
+    );
+  }
+  
